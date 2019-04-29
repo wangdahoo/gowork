7da44c20401d86ecc75dbffe7c8e6f930187acb8
wangdahoo <157195705@qq.com>
2019-04-29 22:42:03 +0800

    add eslint & lint code

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': 'standard',
+  'globals': {
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    "semi": "error",
+    "camelcase": "off",
+    'no-template-curly-in-string': 'off'
+  },
+
+  "overrides": [
+    {
+      "files": ["lib/*.js", ".eslintrc.js"],
+      "excludedFiles": "*.test.js"
+    }
+  ]
+}
